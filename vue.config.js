a5d3f66dd65165412bee5a1ac79ace8b01749388
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-02-13 09:56:07 +0530

    file hash removed

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     runtimeCompiler: true,
     productionSourceMap: false,
-
+    filenameHashing: false,
+	configureWebpack: {
+		output: {
+	   		filename: 'app.js'
+		}
+	},
     publicPath: process.env.NODE_ENV === 'production'
         ? './'
         : '/'
